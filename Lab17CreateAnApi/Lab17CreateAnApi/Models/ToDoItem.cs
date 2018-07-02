0f5a818c940e4041c9b3ad1b530e637931d94b2f
Max <gusstorm132@gmail.com>
2018-07-01 19:00:15 -0700

    have most of the initial scaffolding made

---
+++ Lab17CreateAnApi/Lab17CreateAnApi/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab17CreateAnApi.Models
+{
+    public class ToDoItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; }
+        public int ListID { get; set; }
+    }
+}
